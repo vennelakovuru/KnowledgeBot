7d30026b9633a24bb875babc6fbccf08ad40a570
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-03-02 12:34:57 -0600

    styling alligned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {ChatDialogComponent} from './chat/chat-dialog/chat-dialog.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,6 +15,10 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent
   },
+  {
+    path: 'knowledgebot',
+    component: ChatDialogComponent
+  }
 
 ];
 
