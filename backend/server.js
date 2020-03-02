5596f0567724a71643008062319ec62e8b1b230d
bobbalajagruthi <60417998+bobbalajagruthi@users.noreply.github.com>
2020-03-02 11:20:36 -0600

    storage

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const app = express()
+const server = express()
+const mongoose = require('mongoose');
 
-app.listen(6666, () => {
-  console.log('Server started!')
+
+mongoose.connect('mongodb+srv://jagruthi6:<Infy@@19>@cluster0-8fknu.mongodb.net/test?retryWrites=true&w=majority')
+.then(() => console.log('connection successful'))
+.catch((err) => console.log(err));
+
+const apiRouter = require('./Routes/route');
+app.use('/api', apiRouter);
+
+app.listen(3000, () => {
+  console.log('Server 3000 started!')
 })
