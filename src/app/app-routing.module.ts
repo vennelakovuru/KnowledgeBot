96f6dde09ad0b28f0cddb271a658e938d3786fcf
pooja reddy <poojareddygopu@gmail.com>
2020-03-01 19:06:15 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
 
-const routes: Routes = [];
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
