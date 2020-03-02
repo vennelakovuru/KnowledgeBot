0a2823775f15c2d93679771d1c5ab331131358a0
Harini-32 <60243183+Harini-32@users.noreply.github.com>
2020-03-02 11:35:08 -0500

    register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public email: any;
+  public password: string;
+  public repeatPassword : any;
+  public rememberMe: boolean;
+  public error: string;
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
