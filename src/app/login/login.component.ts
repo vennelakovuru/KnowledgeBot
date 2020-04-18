a1b9abaa56183c1bc0d0b4e2b19beaf2509db72c
Harini-32 <60243183+Harini-32@users.noreply.github.com>
2020-04-17 20:35:51 -0400

    login authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 import {Router} from '@angular/router';
- 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 const apiUrl = '/api';
 
@@ -13,21 +14,50 @@ const apiUrl = '/api';
 
 
 export class LoginComponent implements OnInit {
+
+  registerForm: FormGroup;
+  submitted = false;
   
   email: '';
   password = '';
   showerrmsg = false;
   loginattempted = false;
 
-  constructor(private http: HttpClient, private router: Router) {
-  }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router)
+   { }
 
 
   ngOnInit(): void {
 
-    
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required],
+            acceptTerms: [false, Validators.requiredTrue]
+      
+        });
     
   }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+}
+
+onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+}
 
   login() {
     this.loginattempted = true;
